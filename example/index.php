2180e6904b1aec0a675f20b2bc4a3e01fc102984
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-06-23 21:15:51 -0300

    feature: Implement example

--- example/index.php
+++ example/index.php
@@ -1,2 +1,39 @@
 <?php
 
+use GabrielAnhaia\PhpCircuitBreaker\Adapter\Redis\RedisCircuitBreaker;
+use GabrielAnhaia\PhpCircuitBreaker\CircuitBreaker;
+
+require_once('../vendor/autoload.php');
+require_once('ServiceExample.php');
+
+// You can insert this part in your service container to inject the dependencies.
+$settings = [
+    'exceptions_on' => false,
+    'time_window' => 10,
+    'time_out_open' => 10,
+    'time_out_half_open' => 10,
+    'total_failures' => 5
+];
+$redis = new \Redis;
+$redis->connect('localhost');
+$redisCircuitBreaker = new RedisCircuitBreaker($redis);
+$circuitBreaker = new CircuitBreaker($redisCircuitBreaker, $settings);
+
+$serviceName = 'MICROSERVICE_NAME';
+
+if ($circuitBreaker->canPass($serviceName) !== true) {
+    return;
+}
+
+// First attempt example (success).
+try {
+    // You can change the parameter to below to false and run this code 5 times in less than the "time_window",
+    // If you do that, it will open the circuit and the method "canPass" will return false until the time_out_open runs out.
+    ServiceExample::callServiceExample(false);
+    $circuitBreaker->succeed($serviceName);
+    print_r('SUCCESS');
+} catch (\Exception $exception) {
+    $circuitBreaker->failed($serviceName);
+    print_r('FAIL');
+    return;
+}
