4dd08c3d7145d5f8adf5dd4df4e507f273f7accb
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-07-06 19:59:19 -0300

    feature: Create cofig file and provider

---
+++ config/circuit_breaker.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'driver' => 'redis',
+    'exceptions_on' => false,
+    'time_window' => 20,
+    'time_out_open' => 30,
+    'time_out_half_open' => 20,
+    'total_failures' => 5
+];
---
+++ src/Providers/CircuitBreakerServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace GabrielAnhaia\LaravelCircuitBreaker\Providers;
+
+use GabrielAnhaia\PhpCircuitBreaker\Adapter\Redis\RedisCircuitBreaker;
+use GabrielAnhaia\PhpCircuitBreaker\CircuitBreaker;
+use GabrielAnhaia\PhpCircuitBreaker\Contract\CircuitBreakerAdapter;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class CircuitBreakerServiceProvider
+ *
+ * @package GabrielAnhaia\LaravelCircuitBreaker\Providers
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class CircuitBreakerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register method.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/circuit_breaker.php', 'circuit_breaker'
+        );
+
+        $this->app->bind(CircuitBreakerAdapter::class, RedisCircuitBreaker::class);
+
+        $this->app->bind(CircuitBreaker::class, function ($app) {
+            $settings = [
+                'exceptions_on' => config('circuit_breaker.exceptions_on'),
+                'time_window' => config('circuit_breaker.time_window'),
+                'time_out_open' => config('circuit_breaker.time_out_open'),
+                'time_out_half_open' => config('circuit_breaker.time_out_half_open'),
+                'total_failures' => config('circuit_breaker.total_failures')
+            ];
+
+            return new CircuitBreaker(
+                $this->app->make(CircuitBreakerAdapter::class),
+                $settings
+            );
+        });
+    }
+}
